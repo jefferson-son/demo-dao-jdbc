95b4569bfb9fc8d4a7e47d71b5b4c4e0dbf92195
Jefferson Ferreira da Silva <jeffersonferreira.son.dev@gmail.com>
2023-03-29 15:44:26 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department department = new Department(1, "Book");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.00, department);
+		
 		System.out.println(department);
+		System.out.println(seller);
 
 	}
 
