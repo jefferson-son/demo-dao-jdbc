c0bb539d694e1ec99678c5406333fbf2e8f4f02f
Jefferson Ferreira da Silva <jeffersonferreira.son.dev@gmail.com>
2023-03-29 16:02:34 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 
 		Department department = new Department(1, "Book");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.00, department);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(department);
 		System.out.println(seller);
